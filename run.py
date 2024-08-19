791d29cf82cc34d27b8e85d6671dd302022b146f
Jakob Fender <jakob.fender@hslu.ch>
2024-08-19 18:08:30 +0200

    make strategy selection a parameter

--- run.py
+++ run.py
@@ -9,14 +9,27 @@ from src.trump_strategy.random_trump_strategy import RandomTrumpStrategy
 from src.utils.log_utils import LogUtils
 from src.utils.results_utils import ResultsUtils
 
-POSSIBLE_MODELS = ["random"]
+POSSIBLE_TRUMP_STRATEGIES = ["random"]
+POSSIBLE_PLAY_STRATEGIES = ["random"]
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("-a", "--agent", default=POSSIBLE_MODELS[0], choices=POSSIBLE_MODELS,
-                        help="Choose the bot")
-    parser.add_argument("-o", "--opponent", default=POSSIBLE_MODELS[0], choices=["All"] + POSSIBLE_MODELS,
-                        help="Choose the opponent")
+    parser.add_argument("-at", "--agent-trump-strategy",
+                        default=POSSIBLE_TRUMP_STRATEGIES[0],
+                        choices=POSSIBLE_TRUMP_STRATEGIES,
+                        help="Choose the trump strategy for the agent")
+    parser.add_argument("-ap", "--agent-play-strategy",
+                        default=POSSIBLE_PLAY_STRATEGIES[0],
+                        choices=POSSIBLE_PLAY_STRATEGIES,
+                        help="Choose the play strategy for the agent")
+    parser.add_argument("-ot", "--opponent-trump-strategy",
+                        default=POSSIBLE_TRUMP_STRATEGIES[0],
+                        choices=POSSIBLE_TRUMP_STRATEGIES,
+                        help="Choose the trump strategy for the opponent")
+    parser.add_argument("-op", "--opponent-play-strategy",
+                        default=POSSIBLE_PLAY_STRATEGIES[0],
+                        choices=POSSIBLE_PLAY_STRATEGIES,
+                        help="Choose the play strategy for the opponent")
     parser.add_argument("-n", "--n_games", default=100, type=int, help="Number of games to play")
     parser.add_argument("-ll", "--log_level", default="INFO", choices=["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"],
                         help="Set the logging level")
@@ -30,25 +43,36 @@ if __name__ == "__main__":
     logger = logging.getLogger("run.py")
 
     logger.info("Running a game Simulation with the following parameters:")
-    logger.info(f"Agent: {args.agent}")
-    logger.info(f"Opponent: {args.opponent}")
+    logger.info(f"Agent: {args.agent_trump_strategy} - {args.agent_play_strategy}")
+    logger.info(f"Opponent: {args.opponent_trump_strategy} - {args.opponent_play_strategy}")
     logger.info(f"Number of games: {args.n_games}")
     logger.info(f"Log Level: {args.log_level}")
     logger.info("Starting the simulation...")
 
-    if args.agent == "random":
-        trump_strategy = RandomTrumpStrategy(log_level=args.log_level, seed=args.seed)
-        play_strategy = RandomPlayStrategy(log_level=args.log_level, seed=args.seed)
-        agent_team = [CustomAgent(trump_strategy, play_strategy), CustomAgent(trump_strategy, play_strategy)]
+    agent_trump_strategy = None
+    agent_play_strategy = None
 
-    if args.opponent == "random":
-        trump_strategy = RandomTrumpStrategy(log_level=args.log_level, seed=args.seed)
-        play_strategy = RandomPlayStrategy(log_level=args.log_level, seed=args.seed)
-        opponent_team = [CustomAgent(trump_strategy, play_strategy), CustomAgent(trump_strategy, play_strategy)]
+    if args.agent_trump_strategy == "random":
+        agent_trump_strategy = RandomTrumpStrategy(log_level=args.log_level, seed=args.seed)
+    if args.agent_play_strategy == "random":
+        agent_play_strategy = RandomPlayStrategy(log_level=args.log_level, seed=args.seed)
+
+    opponent_trump_strategy = None
+    opponent_play_strategy = None
+
+    if args.opponent_trump_strategy == "random":
+        opponent_trump_strategy = RandomTrumpStrategy(log_level=args.log_level, seed=args.seed)
+    if args.opponent_play_strategy == "random":
+        opponent_play_strategy = RandomPlayStrategy(log_level=args.log_level, seed=args.seed)
 
     arena = Arena(nr_games_to_play=args.n_games, save_filename=f"logs/{log_utils.formatted_start_time}_arena_logs")
 
-    arena.set_players(agent_team[0], opponent_team[0], agent_team[1], opponent_team[1])
+    arena.set_players(
+        CustomAgent(agent_trump_strategy, agent_play_strategy),
+        CustomAgent(opponent_trump_strategy, opponent_play_strategy),
+        CustomAgent(agent_trump_strategy, agent_play_strategy),
+        CustomAgent(opponent_trump_strategy, opponent_play_strategy)
+    )
     arena.play_all_games()
 
     results_utils.print_results(arena)
