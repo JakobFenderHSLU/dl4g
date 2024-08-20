5e923f5f52da6e9f5d1f5af91d27bffd2ccae7bf
Jakob Fender <jakob.fender@hslu.ch>
2024-08-20 17:59:43 +0200

    refactor ResultsUtils

--- src/utils/results_utils.py
+++ src/utils/results_utils.py
@@ -17,25 +17,24 @@ class ResultsUtils:
         n_team_1_wins = len(arena.points_team_0) - n_team_0_wins
 
         bar_length = 50
-        self._logger.info(f" {n_team_0_wins} "
-                          f"{' ' * (bar_length - len(str(n_team_0_wins)) - len(str(n_team_1_wins)) - 1)}"
-                          f"{n_team_1_wins}")
-        self._logger.info(
-            f"[{'+' * int(n_team_0_wins / arena.nr_games_played * bar_length) + '-' * int(n_team_1_wins / arena.nr_games_played * bar_length)}]")
+        self._print_stacked_bar_plot(bar_length, n_team_0_wins, n_team_1_wins)
 
-        self._logger.info("")
+        agents_points = arena.points_team_0
+        agents_winrate = (arena.points_team_0 > arena.points_team_1).mean() * 100
+        opponents_points = arena.points_team_1
+        opponents_winrate = (arena.points_team_1 > arena.points_team_0).mean() * 100
 
         table = PrettyTable(["Overall", "Agents", "Opponents"])
-        table.add_row(["Total Points", sum(arena.points_team_0), sum(arena.points_team_1)])
-        table.add_row(["Average Points", sum(arena.points_team_0) / arena.nr_games_played,
-                       sum(arena.points_team_1) / arena.nr_games_played])
-        table.add_row(["Max Points", max(arena.points_team_0), max(arena.points_team_1)])
-        table.add_row(["Min Points", min(arena.points_team_0), min(arena.points_team_1)])
-        table.add_row(["Points 25th Percentile", np.quantile(arena.points_team_0, 0.25),
-                       np.quantile(arena.points_team_1, 0.25)])
-        table.add_row(["Points Median", np.median(arena.points_team_0), np.median(arena.points_team_1)])
-        table.add_row(["Points 75th Percentile", np.quantile(arena.points_team_0, 0.75),
-                       np.quantile(arena.points_team_1, 0.75)])
+        table.add_row(["Winrate", f"{agents_winrate:.2f} %", f"{opponents_winrate:.2f} %"])
+        table.add_row(["Average Points", sum(agents_points) / len(agents_points),
+                       sum(opponents_points) / len(opponents_points)])
+        table.add_row(["Max Points", max(agents_points), max(opponents_points)])
+        table.add_row(["Min Points", min(agents_points), min(opponents_points)])
+        table.add_row(["Points 25th Percentile", np.quantile(agents_points, 0.25),
+                       np.quantile(opponents_points, 0.25)])
+        table.add_row(["Points Median", np.median(agents_points), np.median(opponents_points)])
+        table.add_row(["Points 75th Percentile", np.quantile(agents_points, 0.75),
+                       np.quantile(opponents_points, 0.75)])
 
         [self._logger.info(" " + row) for row in table.get_string().split("\n")]
 
@@ -43,10 +42,12 @@ class ResultsUtils:
 
         # get every second row
         agent_trump_rounds = arena.points_team_0[1::2]
+        agent_trump_rounds_winrate = (arena.points_team_0[1::2] > arena.points_team_1[1::2]).mean() * 100
         opponent_trump_rounds = arena.points_team_1[::2]
+        opponent_trump_rounds_winrate = (arena.points_team_1[::2] > arena.points_team_0[::2]).mean() * 100
 
         table = PrettyTable(["Trump Rounds", "Agents", "Opponents"])
-        table.add_row(["Total Points", sum(agent_trump_rounds), sum(opponent_trump_rounds)])
+        table.add_row(["Winrate", f"{agent_trump_rounds_winrate:.2f} %", f"{opponent_trump_rounds_winrate:.2f} %"])
         table.add_row(["Average Points", sum(agent_trump_rounds) / len(agent_trump_rounds),
                        sum(opponent_trump_rounds) / len(opponent_trump_rounds)])
         table.add_row(["Max Points", max(agent_trump_rounds), max(opponent_trump_rounds)])
@@ -58,3 +59,16 @@ class ResultsUtils:
                        np.quantile(opponent_trump_rounds, 0.75)])
 
         [self._logger.info(" " + row) for row in table.get_string().split("\n")]
+
+    def _print_stacked_bar_plot(self, bar_length, agents_wins, opponents_wins):
+        #  659                                           341
+        # [++++++++++++++++++++++++++++++++-----------------]
+        n_games_played = agents_wins + opponents_wins
+        self._logger.info(f" {agents_wins} "
+                          f"{' ' * (bar_length - len(str(agents_wins)) - len(str(opponents_wins)) - 1)}"
+                          f"{opponents_wins}")
+        plus = '+' * int(agents_wins / n_games_played * bar_length)
+        minus = '-' * int(opponents_wins / n_games_played * bar_length)
+        self._logger.info(
+            f"[{plus}{minus}]")
+        self._logger.info("")
