4a9f516bb691d1b02b1525afc78de9139d9cbb09
Jakob Fender <jakob.fender@hslu.ch>
2024-08-20 20:32:28 +0200

    add highest_value_play_strategy.py

---
+++ src/play_strategy/highest_value_play_strategy.py
@@ -0,0 +1,42 @@
+import numpy as np
+from jass.game.const import OBE_ABE, UNE_UFE, DIAMONDS, HEARTS, SPADES, CLUBS
+from jass.game.game_observation import GameObservation
+
+from src.play_strategy.abstract_play_strategy import PlayStrategy
+
+
+class HighestValuePlayStrategy(PlayStrategy):
+    # leave one empty for the cards for current suit cards
+    _trump_weight = [25, 24, 23, 27, 22, 26, 21, 20, 19]
+    _no_trump_weight = [9, 8, 7, 6, 5, 4, 3, 2, 1]
+    _no_trump_weight_current_suit = [18, 17, 16, 15, 14, 13, 12, 11, 10]
+
+    _obenabe_weight = [18, 16, 14, 12, 10, 8, 6, 4, 2]
+    _obenabe_weight_current_suit = [19, 17, 15, 13, 11, 9, 7, 5, 3]
+
+    _uneufe_weight = [2, 4, 6, 8, 10, 12, 14, 16, 18]
+    _uneufe_weight_current_suit = [3, 5, 7, 9, 11, 13, 15, 17, 19]
+
+    def __init__(self, log_level: str, seed: int):
+        super().__init__(log_level, __name__, seed)
+
+    def choose_card(self, observation: GameObservation) -> int:
+        current_trump = observation.trump
+        current_suit = observation.current_trick[0] // 9
+
+        mask = np.zeros(36, np.int32)
+
+        if current_trump in [DIAMONDS, HEARTS, SPADES, CLUBS]:
+            mask = np.tile(self._no_trump_weight, 4)
+            mask[current_suit * 9:current_suit * 9 + 9] = self._no_trump_weight_current_suit
+            mask[current_trump * 9:current_trump * 9 + 9] = self._trump_weight
+        if current_trump == OBE_ABE:
+            mask = np.tile(self._obenabe_weight, 4)
+            mask[current_suit * 9:current_suit * 9 + 9] = self._obenabe_weight_current_suit
+        elif current_trump == UNE_UFE:
+            mask = np.tile(self._uneufe_weight, 4)
+            mask[current_suit * 9:current_suit * 9 + 9] = self._uneufe_weight_current_suit
+
+        possible_cards = self._rule.get_valid_cards_from_obs(observation)
+
+        return int(np.argmax(possible_cards * mask))
---
+++ test_src/play_strategy/highest_value_play_strategy_test.py
@@ -0,0 +1,179 @@
+import unittest
+
+import numpy as np
+from jass.game.game_observation import GameObservation
+
+from play_strategy.highest_value_play_strategy import HighestValuePlayStrategy
+
+
+class HighestValuePlayStrategyTest(unittest.TestCase):
+
+    def setUp(self):
+        self._log_level = "DEBUG"
+        self._seed = 42
+        self._strategy = HighestValuePlayStrategy(log_level=self._log_level, seed=self._seed)
+
+    def test_choose_suit_highest(self):
+        observation = GameObservation()
+
+        observation.trump = 3  # CLUB
+        hand = np.zeros(36)
+        hand[5] = 1  # DIAMOND 9
+        hand[17] = 1  # HEART 6
+        hand[19] = 1  # SPADE KING
+        hand[27] = 1  # CLUB ACE
+
+        observation.hand = hand
+        observation.current_trick = [7, 0, 0, 0]
+
+        # ALL CARDS ARE VALID
+        self._strategy._rule.get_valid_cards_from_obs = lambda x: hand
+        card = self._strategy.choose_card(observation)
+        self.assertEqual(27, card)
+
+    def test_choose_suit_normal(self):
+        observation = GameObservation()
+
+        observation.trump = 1  # HEART
+        hand = np.zeros(36)
+        hand[5] = 1  # DIAMOND 9
+        hand[17] = 1  # HEART 6
+        hand[19] = 1  # SPADE KING
+        hand[27] = 1  # CLUB ACE
+
+        observation.hand = hand
+        observation.current_trick = [7, 0, 0, 0]
+
+        # ALL CARDS ARE VALID
+        self._strategy._rule.get_valid_cards_from_obs = lambda x: hand
+        card = self._strategy.choose_card(observation)
+        self.assertEqual(17, card)
+
+    def test_choose_suit_tie(self):
+        observation = GameObservation()
+
+        observation.trump = 1  # DIAMOND
+        hand = np.zeros(36)
+        hand[10] = 1  # HEART KING
+        hand[19] = 1  # SPADE KING
+        hand[27] = 1  # CLUB ACE
+
+        observation.hand = hand
+        observation.current_trick = [11, 0, 0, 0]
+
+        # ALL CARDS ARE VALID
+        self._strategy._rule.get_valid_cards_from_obs = lambda x: hand
+        card = self._strategy.choose_card(observation)
+        self.assertEqual(10, card)
+
+    def test_choose_obenabe(self):
+        observation = GameObservation()
+
+        observation.trump = 4  # OBE_ABE
+        hand = np.zeros(36)
+        hand[5] = 1  # DIAMOND 9
+        hand[17] = 1  # HEART 6
+        hand[19] = 1  # SPADE KING
+        hand[27] = 1  # CLUB ACE
+
+        observation.hand = hand
+        observation.current_trick = [22, 0, 0, 0]
+
+        # ALL CARDS ARE VALID
+        self._strategy._rule.get_valid_cards_from_obs = lambda x: hand
+        card = self._strategy.choose_card(observation)
+        self.assertEqual(27, card)
+
+    def test_choose_obenabe_suit(self):
+        observation = GameObservation()
+
+        observation.trump = 4  # OBE_ABE
+        hand = np.zeros(36)
+        hand[5] = 1  # DIAMOND 9
+        hand[17] = 1  # HEART 6
+        hand[19] = 1  # SPADE KING
+        hand[18] = 1  # SPADE ACE
+        hand[27] = 1  # CLUB ACE
+
+        observation.hand = hand
+        observation.current_trick = [22, 0, 0, 0]
+
+        # ALL CARDS ARE VALID
+        self._strategy._rule.get_valid_cards_from_obs = lambda x: hand
+        card = self._strategy.choose_card(observation)
+        self.assertEqual(18, card)
+
+    def test_choose_obenabe_tie(self):
+        observation = GameObservation()
+
+        observation.trump = 4  # OBE_ABE
+        hand = np.zeros(36)
+        hand[5] = 1  # DIAMOND 9
+        hand[17] = 1  # HEART 6
+        hand[19] = 1  # SPADE KING
+        hand[18] = 1  # SPADE ACE
+        hand[27] = 1  # CLUB ACE
+
+        observation.hand = hand
+        observation.current_trick = [3, 0, 0, 0]
+
+        # ALL CARDS ARE VALID
+        self._strategy._rule.get_valid_cards_from_obs = lambda x: hand
+        card = self._strategy.choose_card(observation)
+        self.assertEqual(18, card)
+
+    def test_choose_suit_uneufe(self):
+        observation = GameObservation()
+
+        observation.trump = 5  # UNE_UFE
+        hand = np.zeros(36)
+        hand[5] = 1  # DIAMOND 9
+        hand[17] = 1  # HEART 6
+        hand[19] = 1  # SPADE KING
+        hand[27] = 1  # CLUB ACE
+
+        observation.hand = hand
+        observation.current_trick = [22, 0, 0, 0]
+
+        # ALL CARDS ARE VALID
+        self._strategy._rule.get_valid_cards_from_obs = lambda x: hand
+        card = self._strategy.choose_card(observation)
+        self.assertEqual(17, card)
+
+    def test_choose_suit_uneufe_suit(self):
+        observation = GameObservation()
+
+        observation.trump = 5  # UNE_UFE
+        hand = np.zeros(36)
+        hand[5] = 1  # DIAMOND 9
+        hand[17] = 1  # HEART 6
+        hand[19] = 1  # SPADE KING
+        hand[26] = 1  # SPADE 6
+        hand[27] = 1  # CLUB ACE
+
+        observation.hand = hand
+        observation.current_trick = [22, 0, 0, 0]
+
+        # ALL CARDS ARE VALID
+        self._strategy._rule.get_valid_cards_from_obs = lambda x: hand
+        card = self._strategy.choose_card(observation)
+        self.assertEqual(26, card)
+
+    def test_choose_suit_uneufe_tie(self):
+        observation = GameObservation()
+
+        observation.trump = 5  # UNE_UFE
+        hand = np.zeros(36)
+        hand[5] = 1  # DIAMOND 9
+        hand[17] = 1  # HEART 6
+        hand[19] = 1  # SPADE KING
+        hand[26] = 1  # SPADE 6
+        hand[27] = 1  # CLUB ACE
+
+        observation.hand = hand
+        observation.current_trick = [3, 0, 0, 0]
+
+        # ALL CARDS ARE VALID
+        self._strategy._rule.get_valid_cards_from_obs = lambda x: hand
+        card = self._strategy.choose_card(observation)
+        self.assertEqual(17, card)
