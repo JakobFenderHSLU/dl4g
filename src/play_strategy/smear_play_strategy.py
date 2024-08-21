9b0434162827760b657e5ca76335fd0c708e163a
Jakob Fender <jakob.fender@hslu.ch>
2024-08-21 22:38:24 +0200

    add smear_play_strategy.py

---
+++ src/play_strategy/smear_play_strategy.py
@@ -0,0 +1,39 @@
+import numpy as np
+from jass.game.const import OBE_ABE
+
+import src.utils.game_utils as gu
+from play_strategy.abstract_play_strategy import PlayStrategy
+
+
+class SmearPlayStrategy(PlayStrategy):
+    _smear_weight = [0, 0, 0, 0, 1, 0, 0, 0, 0]
+    _obenabe_smear_weight = [0, 0, 0, 0, 2, 0, 1, 0, 0]
+
+    def __init__(self, log_level: str, seed: int, next_strategy: PlayStrategy):
+        super().__init__(log_level, __name__, seed)
+        self._next_strategy = next_strategy
+
+    def choose_card(self, observation) -> int:
+        # Skip if not last card in trick
+        if observation.nr_cards_in_trick != 3:
+            return self._next_strategy.choose_card(observation)
+
+        # Skip if the trick is not safe
+        if not gu.is_safe_trick(observation, self._rule):
+            return self._next_strategy.choose_card(observation)
+
+        # Get valid cards
+        valid_cards = self._rule.get_valid_cards_from_obs(observation)
+
+        if observation.trump == OBE_ABE:
+            mask = np.tile(self._obenabe_smear_weight, 4)
+        else:
+            mask = np.tile(self._smear_weight, 4)
+
+        possible_cards = valid_cards * mask
+
+        # Skip if no card to smear
+        if sum(possible_cards) == 0:
+            return self._next_strategy.choose_card(observation)
+
+        return int(np.argmax(possible_cards))
---
+++ src/play_strategy/test_smear_play_strategy.py
@@ -0,0 +1,99 @@
+from unittest import TestCase
+
+import numpy as np
+from jass.game.const import OBE_ABE, UNE_UFE, DIAMONDS
+from jass.game.game_observation import GameObservation
+
+from play_strategy.mock_play_strategy import MockPlayStrategy
+from src.play_strategy.smear_play_strategy import SmearPlayStrategy
+
+
+class TestSmearPlayStrategy(TestCase):
+    def setUp(self):
+        self._log_level = "DEBUG"
+        self._seed = 42
+        mock_strategy = MockPlayStrategy(self._log_level, self._seed, 99)
+        self._strategy = SmearPlayStrategy(log_level=self._log_level, seed=self._seed, next_strategy=mock_strategy)
+
+    def test_choose_card_not_last(self):
+        obs = GameObservation()
+        obs.current_trick = np.array([7, 1, -1, -1])
+
+        card = self._strategy.choose_card(obs)
+        self.assertEqual(card, 99)
+
+    def test_choose_card_not_save(self):
+        obs = GameObservation()
+        obs.trump = 0
+        obs.current_trick = np.array([7, 1, 5, -1])
+
+        card = self._strategy.choose_card(obs)
+        self.assertEqual(card, 99)
+
+    def test_choose_card_smear_obe_abe(self):
+        obs = GameObservation()
+        obs.trump = OBE_ABE
+        obs.player = 0
+        valid_cards = np.zeros(36)
+        valid_cards[5] = 1  # DIAMOND 9
+        valid_cards[15] = 1  # HEART 8
+        valid_cards[26] = 1  # SPADE 6
+
+        obs.current_trick = np.array([7, 0, 9, -1])
+        obs.nr_cards_in_trick = 3
+
+        self._strategy._rule.get_valid_cards_from_obs = lambda x: valid_cards
+
+        card = self._strategy.choose_card(obs)
+        self.assertEqual(card, 15)
+
+    def test_choose_card_smear_une_ufe(self):
+        obs = GameObservation()
+        obs.trump = UNE_UFE
+        obs.player = 0
+        valid_cards = np.zeros(36)
+        valid_cards[4] = 1  # DIAMOND 10
+        valid_cards[15] = 1  # HEART 8
+        valid_cards[26] = 1  # SPADE 6
+
+        obs.current_trick = np.array([7, 8, 9, -1])
+        obs.nr_cards_in_trick = 3
+
+        self._strategy._rule.get_valid_cards_from_obs = lambda x: valid_cards
+
+        card = self._strategy.choose_card(obs)
+        self.assertEqual(card, 4)
+
+    def test_choose_card_smear_trump(self):
+        obs = GameObservation()
+        obs.trump = DIAMONDS
+        obs.player = 0
+        valid_cards = np.zeros(36)
+        valid_cards[4] = 1  # DIAMOND 10
+        valid_cards[15] = 1  # HEART 8
+        valid_cards[26] = 1  # SPADE 6
+
+        obs.current_trick = np.array([4, 3, 0, -1])
+        obs.nr_cards_in_trick = 3
+
+        self._strategy._rule.get_valid_cards_from_obs = lambda x: valid_cards
+
+        card = self._strategy.choose_card(obs)
+        self.assertEqual(card, 4)
+
+    def test_choose_card_smear_no_card(self):
+        obs = GameObservation()
+        obs.trump = UNE_UFE
+        obs.player = 0
+        valid_cards = np.zeros(36)
+        valid_cards[5] = 1  # DIAMOND 9
+        valid_cards[15] = 1  # HEART 8
+        valid_cards[26] = 1  # SPADE 6
+
+        obs.current_trick = np.array([7, 8, 9, -1])
+        obs.nr_cards_in_trick = 3
+
+        self._strategy._rule.get_valid_cards_from_obs = lambda x: valid_cards
+
+        card = self._strategy.choose_card(obs)
+        self.assertEqual(card, 99)
