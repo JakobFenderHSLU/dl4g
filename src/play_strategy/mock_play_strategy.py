d743f6b6a7e7986076642c974242c386f2278241
Jakob Fender <jakob.fender@hslu.ch>
2024-08-21 20:06:00 +0200

    add mock_play_strategy.py

---
+++ src/play_strategy/mock_play_strategy.py
@@ -0,0 +1,12 @@
+from jass.game.game_observation import GameObservation
+
+from play_strategy.abstract_play_strategy import PlayStrategy
+
+
+class MockPlayStrategy(PlayStrategy):
+    def __init__(self, log_level: str, seed: int, play: int):
+        super().__init__(log_level, __name__, seed)
+        self.play = play
+
+    def choose_card(self, observation: GameObservation) -> int:
+        return self.play
