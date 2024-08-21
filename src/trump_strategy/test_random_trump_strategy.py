847d67f6ae15fbe4a1183436217d36829b26c624
Jakob Fender <jakob.fender@hslu.ch>
2024-08-21 18:08:53 +0200

    add test_random_trump_strategy.py

---
+++ src/trump_strategy/test_random_trump_strategy.py
@@ -0,0 +1,14 @@
+from unittest import TestCase
+
+from src.trump_strategy.random_trump_strategy import RandomTrumpStrategy
+
+
+class TestRandomTrumpStrategy(TestCase):
+    def setUp(self):
+        self._log_level = "DEBUG"
+        self._seed = 42
+        self._strategy = RandomTrumpStrategy(log_level=self._log_level, seed=self._seed)
+
+    def test_choose_trump(self):
+        trump = self._strategy.choose_trump(None)
+        self.assertTrue(0 <= trump <= 3)
