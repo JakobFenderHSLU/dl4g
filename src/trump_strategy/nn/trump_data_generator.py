63a07f6d3de63ef60bf138a8330506d5511f7738
Jakob Fender <jakob.fender@hslu.ch>
2024-08-24 21:13:08 +0200

    implement caching

--- src/trump_strategy/nn/trump_data_generator.py
+++ src/trump_strategy/nn/trump_data_generator.py
@@ -1,7 +1,7 @@
-from typing import Tuple
+import logging
+import os
 
 import numpy as np
-import pandas as pd
 from jass.game.const import MAX_TRUMP
 from jass.game.game_sim import GameSim
 from jass.game.game_state import GameState
@@ -13,72 +13,85 @@ import src.utils.game_utils as gu
 
 
 class TrumpDataGenerator:
-    def __init__(self, load_data=False, n_play_per_hand=100, backup_interval=1000, verbose=False):
-        self.existing_deck = set()
-        self.n_yielded_hands = 0
-        self.deck = np.arange(36)
+    def __init__(self, load_data=False, n_play_per_hand=100, backup_interval=1000, cache_size=1_000_000):
+        self.cache_size = cache_size
+        self.cached_decks = np.zeros((self.cache_size, 36))
+        self.cached_results = np.zeros((self.cache_size, MAX_TRUMP + 1, n_play_per_hand))
         self.n_play_per_hand = n_play_per_hand
         self.backup_interval = backup_interval
-        self.verbose = verbose
+
+        self.n_cached_results = 0
+        self.n_yielded_hands = 0
+
+        self.deck = np.arange(36)
 
         self.game = GameSim(RuleSchieber())
         self.game_state = GameState()
         self.game_state.dealer = 0
         self.game_state.player = 1
 
-        self.cached_decks = pd.DataFrame(columns=[
-            "hand", "deck", "score"
-        ])
+        self.logger = logging.getLogger(__name__)
 
-        # if load_data:
-        #     if os.path.exists("data/existing_hands.pickle"):
-        #         with open("data/existing_hands.pickle", "rb") as f:
-        #             self.existing_deck = pickle.load(f)
-        #
-        #     if os.path.exists("data/cached_hands.csv"):
-        #         with open("data/cached_hands.csv", "r") as f:
-        #             self.cached_decks = pd.read_csv(f)
+        if load_data:
+            if not os.path.exists("data/cached_decks.npy") or not os.path.exists("data/cached_results.npy"):
+                self.logger.warning("no cached data found")
+                return
+
+            with open("data/cached_decks.npy", "rb") as f:
+                loaded_decks = np.load(f)
+
+            with open("data/cached_results.npy", "rb") as f:
+                loaded_results = np.load(f)
+
+            if loaded_decks.shape[0] != loaded_results.shape[0]:
+                self.logger.warning("cached data is inconsistent")
+                return
+
+            self.n_cached_results = loaded_results.shape[0]
+
+            if self.n_cached_results > self.cache_size:
+                self.cached_results = np.zeros(((loaded_results // self.cache_size + 1) * self.cache_size, 36))
+                self.cached_results = np.zeros((
+                    (loaded_results // self.cache_size + 1) * self.cache_size, MAX_TRUMP + 1, n_play_per_hand
+                ))
+
+            self.cached_decks[:self.n_cached_results] = loaded_decks
+            self.cached_results[:self.n_cached_results] = loaded_results
 
     def __iter__(self):
         return self
 
     def __next__(self):
-        # if self.n_yielded_hands % self.backup_interval == 0 and self.n_yielded_hands > 0:
-        #     print(f"Backing up hands at {self.n_yielded_hands}...")
-        #     self._backup_hands()
+        if self.n_yielded_hands % self.backup_interval == 0 and self.n_yielded_hands > 0:
+            self.logger.info(f"Backing up hands at {self.n_yielded_hands}...")
+            self._backup_hands()
 
-        if self.n_yielded_hands < len(self.cached_decks):
-            print(f"Yielding cached hand {self.n_yielded_hands}...")
-            hand = self.cached_decks.iloc[self.n_yielded_hands][:-1].values
+        if self.n_yielded_hands < self.n_cached_results:
+            deck = self.cached_results[self.n_yielded_hands]
+            results = self.cached_results[self.n_yielded_hands]
             self.n_yielded_hands += 1
-            return hand
+            return deck, results
 
-        deck = self._generate_unique_deck()
-        verbose_results = self._get_scores(deck)
+        # Note: No check for uniqueness needed. Because the amount of data generated to have a 0.1% chance of a
+        # duplicate is ~4.33 * 10^15. So we can safely ignore this case.
+        deck = self._generate_random_deck()
+        results = self._get_scores(deck)
 
-        # cache the result
-        # self.cached_decks.loc[self.n_yielded_hands] = result
+        self.cached_decks[self.n_yielded_hands] = deck
+        self.cached_results[self.n_yielded_hands] = results
+
         self.n_yielded_hands += 1
 
-        return deck, verbose_results
+        return deck, results
 
-    # def _backup_hands(self):
-    #     with open("data/existing_hands.pickle", "wb") as f:
-    #         pickle.dump(self.existing_deck, f)
-    #
-    #     # with open("data/cached_hands.csv", "a") as f:
-    #     #     self.cached_decks.to_csv(f, index=False, header=False)
+    def _backup_hands(self):
+        with open("data/cached_decks.npy", "wb") as f:
+            np.savetxt(f, self.cached_decks, fmt="%d")
 
-    def _generate_unique_deck(self) -> np.ndarray:
-        deck, deck_str = self._generate_random_deck()
+        with open("data/cached_results.npy", "wb") as f:
+            np.savetxt(f, self.cached_results, fmt="%d")
 
-        while deck_str in self.existing_deck:
-            deck, deck_str = self._generate_random_deck()
-
-        self.existing_deck.add(deck_str)
-        return deck
-
-    def _generate_random_deck(self) -> Tuple[np.ndarray, str]:
+    def _generate_random_deck(self) -> ndarray:
         deck = self.deck.copy()
         np.random.shuffle(deck)
         hands = gu.deck_to_onehot_hands(deck)
@@ -90,8 +103,7 @@ class TrumpDataGenerator:
             gu.swap_colors_from_order(hands[3], color_order)
         ])
 
-        deck_str = "".join(swapped_deck.flatten().astype(str))
-        return deck, deck_str
+        return swapped_deck
 
     def _get_scores(self, deck: np.ndarray) -> ndarray:
         self.game_state.hands = gu.deck_to_onehot_hands(deck)
@@ -115,7 +127,6 @@ class TrumpDataGenerator:
                     self.game.action_play_card(card_action)
 
                 score[i] = np.where(self.game.state.trick_winner == 0, self.game.state.trick_points, 0).sum()
-                score_for_player_0 = self.game.state.points[0]
 
             results[trump] = score
 
