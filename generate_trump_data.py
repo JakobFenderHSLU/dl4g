210fb2379de5000846a30826003da56ac861e1bb
Jakob Fender <jakob.fender@hslu.ch>
2024-08-25 14:45:29 +0200

    add support for splitting into multiple files

--- generate_trump_data.py
+++ generate_trump_data.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     parser.add_argument("--n-play-per-hand",
                         default=20, type=int, help="Number of games to play per hand")
     parser.add_argument("--backup-interval", default=1000, type=int, help="Interval to backup the data")
-    parser.add_argument("--cache-size", default=1_000_000, type=int, help="Size of the cache")
+    parser.add_argument("--max-cache-size", default=1_000_000, type=int, help="Size of the cache")
     parser.add_argument("--log_level", default="INFO", choices=["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"],
                         help="Set the logging level")
     parser.add_argument("-s", "--seed", type=int, help="Set the seed for the random number generator")
@@ -35,7 +35,7 @@ if __name__ == "__main__":
         load_data=args.load_data,
         n_play_per_hand=args.n_play_per_hand,
         backup_interval=args.backup_interval,
-        cache_size=args.cache_size
+        max_cache_size=args.max_cache_size
     )
 
     for _ in tqdm(range(args.n_hands)):
--- src/trump_strategy/nn/trump_data_generator.py
+++ src/trump_strategy/nn/trump_data_generator.py
@@ -13,15 +13,17 @@ import src.utils.game_utils as gu
 
 
 class TrumpDataGenerator:
-    def __init__(self, load_data=False, n_play_per_hand=100, backup_interval=1000, cache_size=1_000_000):
-        self.cache_size = cache_size
-        self.cached_decks = np.zeros((self.cache_size, 36)).astype(int)
-        self.cached_results = np.zeros((self.cache_size, MAX_TRUMP + 1, n_play_per_hand)).astype(int)
+    def __init__(self, load_data=False, n_play_per_hand=100, backup_interval=10_000, max_cache_size=1_000_000):
+        self.DATA_PATH = "data/trump_data_generator"
+
+        self.max_cache_size = max_cache_size
         self.n_play_per_hand = n_play_per_hand
         self.backup_interval = backup_interval
 
-        self.n_cached_results = 0
-        self.n_yielded_hands = 0
+        self.total_n_cached_results = 0
+        self.relative_n_cached_results = 0
+        self.total_n_yielded_hands = 0
+        self.relative_n_yielded_hands = 0
 
         self.deck = np.arange(36)
 
@@ -33,61 +35,75 @@ class TrumpDataGenerator:
         self.logger = logging.getLogger(__name__)
 
         if load_data:
-            if not os.path.exists("data/cached_decks.npy") or not os.path.exists("data/cached_results.npy"):
-                self.logger.warning("no cached data found")
-                return
-
-            # load as int
-            loaded_decks = np.load("data/cached_decks.npy").astype(int)
-            loaded_results = np.load("data/cached_results.npy").astype(int)
-
-            if loaded_decks.shape[0] != loaded_results.shape[0]:
-                self.logger.warning("cached data is inconsistent")
-                return
-
-            self.n_cached_results = loaded_results.shape[0]
-
-            if self.n_cached_results > self.cache_size:
-                self.cached_results = np.zeros(((loaded_results // self.cache_size + 1) * self.cache_size, 36))
-                self.cached_results = np.zeros((
-                    (loaded_results // self.cache_size + 1) * self.cache_size, MAX_TRUMP + 1, n_play_per_hand
-                ))
-
-            self.cached_decks[:self.n_cached_results] = loaded_decks
-            self.cached_results[:self.n_cached_results] = loaded_results
+            self.cached_decks, self.cached_results = self._load_data()
+        else:
+            self.cached_decks = np.zeros((self.max_cache_size, 36)).astype(int)
+            self.cached_results = np.zeros((self.max_cache_size, MAX_TRUMP + 1, n_play_per_hand)).astype(int)
 
     def __iter__(self):
         return self
 
     def __next__(self):
-        if self.n_yielded_hands % self.backup_interval == 0 and self.n_yielded_hands > 0:
-            self.logger.info(f"Backing up hands at {self.n_yielded_hands}...")
-            self._backup_hands()
+        if self.total_n_yielded_hands >= self.max_cache_size:
+            self._load_data()
+            self.relative_n_yielded_hands = 0
 
-        if self.n_yielded_hands < self.n_cached_results:
-            deck = self.cached_decks[self.n_yielded_hands]
+        # If we have cached data, return it
+        if self.total_n_yielded_hands < self.total_n_cached_results:
+            deck = self.cached_decks[self.relative_n_yielded_hands]
             onehot_hands = gu.deck_to_onehot_hands(deck)
-            results = self.cached_results[self.n_yielded_hands]
-            self.n_yielded_hands += 1
+            results = self.cached_results[self.relative_n_yielded_hands]
             return onehot_hands, results
+        # Backup data at interval if it was generated.
+        elif self.total_n_yielded_hands % self.backup_interval == 0 and self.total_n_yielded_hands > 0:
+            self.logger.info(f"Backing up hands at {self.total_n_yielded_hands}...")
+            self._backup_hands()
 
         # Note: No check for uniqueness needed. Because the amount of data generated to have a 0.1% chance of a
         # duplicate is ~4.33 * 10^15. So we can safely ignore this case.
         onehot_hands = self._generate_random_deck()
         results = self._get_scores(onehot_hands)
 
-        self.cached_decks[self.n_yielded_hands] = np.where(onehot_hands == 1)[1]
-        self.cached_results[self.n_yielded_hands] = results
+        self.cached_decks[self.relative_n_yielded_hands] = np.where(onehot_hands == 1)[1]
+        self.cached_results[self.relative_n_yielded_hands] = results
 
-        self.n_yielded_hands += 1
+        self.total_n_yielded_hands += 1
+        self.relative_n_yielded_hands += 1
 
         return onehot_hands, results
 
+    def _load_data(self):
+        cache_to_load = self.total_n_yielded_hands // self.max_cache_size
+        if (not os.path.exists(f"{self.DATA_PATH}/cached_decks_{cache_to_load}.npy")
+                or not os.path.exists(f"{self.DATA_PATH}/cached_results_{cache_to_load}.npy")):
+            self.logger.warning("no cached data found")
+            return
+
+        # load as int
+        loaded_decks = np.load(f"{self.DATA_PATH}/cached_decks_{cache_to_load}.npy").astype(int)
+        loaded_results = np.load(f"{self.DATA_PATH}/cached_results_{cache_to_load}.npy").astype(int)
+
+        if loaded_decks.shape[0] != loaded_results.shape[0]:
+            self.logger.warning("cached data is inconsistent")
+            return
+
+        self.total_n_cached_results += loaded_results.shape[0]
+        self.relative_n_cached_results = loaded_results.shape[0]
+
+        sized_decks = np.zeros((self.max_cache_size, 36)).astype(int)
+        sized_results = np.zeros((self.max_cache_size, MAX_TRUMP + 1, self.n_play_per_hand)).astype(int)
+
+        sized_decks[:self.relative_n_cached_results] = loaded_decks
+        sized_results[:self.relative_n_cached_results] = loaded_results
+
+        return sized_decks, sized_results
+
     def _backup_hands(self):
-        with open("data/cached_decks.npy", "wb") as f:
+        with open(f"{self.DATA_PATH}/cached_decks_{self.total_n_yielded_hands // self.max_cache_size}.npy", "wb") as f:
             np.save(f, self.cached_decks.astype(int))
 
-        with open("data/cached_results.npy", "wb") as f:
+        with open(f"{self.DATA_PATH}/cached_results_{self.total_n_yielded_hands // self.max_cache_size}.npy",
+                  "wb") as f:
             np.save(f, self.cached_results.astype(int))
 
     def _generate_random_deck(self) -> ndarray:
