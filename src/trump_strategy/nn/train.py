9283268df3205e84a16de234b2c3c250fb7d41e0
Jakob Fender <jakob.fender@hslu.ch>
2024-08-25 16:16:09 +0200

    prelim train changes

--- src/trump_strategy/nn/train.py
+++ src/trump_strategy/nn/train.py
@@ -1,20 +1,34 @@
 import torch
 import torch.nn as nn
+import wandb
 
 from src.trump_strategy.nn.trump_selector import TrumpSelector
+from trump_strategy.nn.trump_data_generator import TrumpDataGenerator
 
 
 class Trainer:
-    def __init__(self, batch_size=64, lr=1e-3):
+    def __init__(self, data_generator: TrumpDataGenerator, batch_size: int, lr: float, weight_decay: float):
         self.batch_size = batch_size
-        self.lr = lr
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.model = TrumpSelector().to(self.device)
-        self.optimizer = torch.optim.Adam(self.model.parameters(), lr=lr)
-        self.loss_fn = nn.CrossEntropyLoss()
+        self.data_generator = data_generator
+
+        self.run = wandb.init(
+            project="dl4g-trump-selection",
+            name=f"bs={batch_size} lr={lr} wd={weight_decay}",
+            config={
+                "batch_size": self.batch_size,
+                "lr": lr,
+                "weight_decay": weight_decay,
+            }
+        )
 
     def train(self):
         print("Training...")
 
-        for epoch in range(self.max_epochs):
-            print(f"Epoch {epoch + 1}/{self.max_epochs}")
+        optimizer = torch.optim.Adam(
+            self.model.parameters(),
+            lr=self.run.config.lr,
+            weight_decay=self.run.config.weight_decay
+        )
+        loss_fn = nn.MSELoss()
