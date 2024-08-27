869b17189dc3c1e5743db0096f849e3b1e6287d6
Jakob Fender <jakob.fender@hslu.ch>
2024-08-27 16:17:07 +0200

    add PUSH to deep_nn_trump_strategy.py

--- src/trump_strategy/deep_nn_trump_strategy.py
+++ src/trump_strategy/deep_nn_trump_strategy.py
@@ -1,4 +1,5 @@
 import torch
+from jass.game.const import PUSH
 from jass.game.game_observation import GameObservation
 
 from trump_strategy.abstract_trump_strategy import TrumpStrategy
@@ -17,5 +18,8 @@ class DeepNNTrumpStrategy(TrumpStrategy):
     def choose_trump(self, observation: GameObservation) -> int:
         hand = observation.hand
         hand = torch.tensor(hand, dtype=torch.float32).unsqueeze(0)
-        trump = self.model(hand).argmax().item()
-        return trump
+        expected_points = self.model(hand)[0]
+        best_trump = expected_points.argmax().item()
+        if observation.forehand == -1 and expected_points[best_trump] < 69:
+            return PUSH
+        return best_trump
