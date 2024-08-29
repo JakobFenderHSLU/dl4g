fe08db990a94ce7a9093b124d75bde24ff12875e
Jakob Fender <jakob.fender@hslu.ch>
2024-08-29 19:08:45 +0200

    init random_play_strategy.py so

--- src/play_strategy/random_play_strategy.py
+++ src/play_strategy/random_play_strategy.py
@@ -5,10 +5,9 @@ from src.play_strategy.abstract_play_strategy import PlayStrategy
 
 
 class RandomPlayStrategy(PlayStrategy):
-    def __init__(self, seed: int):
+    def __init__(self, seed: int = None):
         super().__init__(__name__)
         self._rng = np.random.default_rng(seed)
-
 
     def choose_card(self, observation: GameObservation) -> int:
         valid_cards = self._rule.get_valid_cards_from_obs(observation)
