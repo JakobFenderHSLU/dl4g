64f612123fa95364d22c2560c37747759e84495a
Jakob Fender <jakob.fender@hslu.ch>
2024-11-05 16:38:52 +0100

    move trump weight and value to consts.py

--- src/play_rule_strategy/trump_jack_strategy.py
+++ src/play_rule_strategy/trump_jack_strategy.py
@@ -2,21 +2,11 @@ import numpy as np
 from jass.game.game_observation import GameObservation
 
 from play_rule_strategy.abstract_play_rule import PlayRuleStrategy
+from utils.consts import TRUMP_VALUE_MASK
 from utils.game_utils import is_safe_trick
 
 
 class TrumpJackPlayRuleStrategy(PlayRuleStrategy):
-    _trump_suit_value = [11, 4, 3, 20, 10, 14, 0, 0, 0]
-    _no_trump_suit_value = [11, 4, 3, 2, 10, 0, 0, 0, 0]
-
-    _trump_value_mask = np.tile(_no_trump_suit_value * 4, (6, 1))
-
-    _trump_value_mask[4] = [11, 3, 4, 2, 10, 0, 8, 0, 0] * 4
-    _trump_value_mask[5] = [0, 3, 4, 2, 10, 0, 8, 0, 11] * 4
-
-    for i in range(4):
-        _trump_value_mask[i, i * 9:i * 9 + 9] = _trump_suit_value
-
     def __init__(self, log_level: str, seed: int):
         super().__init__(log_level, "TrumpJackPlayRuleStrategy", seed)
 
@@ -40,5 +30,5 @@ class TrumpJackPlayRuleStrategy(PlayRuleStrategy):
         # if trump has been played, or we have it, and we won't win the trick by default
         # and the current trick is worth more than 20 points, play the trump jack
         if ((np.any(trump_nine in obs.tricks) or obs.hand[trump_nine] == 1) and not is_safe_trick(obs)
-                and np.sum(self._trump_value_mask[obs.trump][obs.current_trick]) > 20):
+                and np.sum(TRUMP_VALUE_MASK[obs.trump][obs.current_trick]) > 20):
             return trump_jack
