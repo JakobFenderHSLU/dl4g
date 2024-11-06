4c8bc23c8b2c2f2b16520ae690c0dd19fddae985
Jakob Fender <jakob.fender@hslu.ch>
2024-11-06 17:48:49 +0100

    extract dmcts logic to worker

--- src/play_strategy/determinized_mcts_play_strategy.py
+++ src/play_strategy/determinized_mcts_play_strategy.py
@@ -1,59 +1,27 @@
 from concurrent.futures.process import ProcessPoolExecutor
-from multiprocessing import Manager
 
 import numpy as np
 from jass.game.game_observation import GameObservation
-from jass.game.game_sim import GameSim
-from jass.game.game_state_util import state_from_observation
-from jass.game.rule_schieber import RuleSchieber
 
+from play_strategy.nn.mcts.dmcts_worker import DMCTSWorker
 from src.play_strategy.abstract_play_strategy import PlayStrategy
 from src.play_strategy.nn.mcts.hand_sampler import HandSampler
-from src.play_strategy.nn.mcts.mcts_tree import MCTS
 
 
 class DeterminizedMCTSPlayStrategy(PlayStrategy):
-    def __init__(self, limit_s=1, n_threads=16):
+    def __init__(self, limit_s):
         super().__init__(__name__)
-        self.n_threads = n_threads
         self.limit_s = limit_s
         self.hand_sampler = HandSampler()
         self.executor = ProcessPoolExecutor()
+        self.dmcts_worker = DMCTSWorker(limit_s)
 
     def choose_card(self, obs: GameObservation) -> int:
-        with Manager() as manager:
-            action_scores = manager.Queue()
-            valid_cards = np.flatnonzero(self._rule.get_valid_cards_from_obs(obs))
+        valid_cards = np.flatnonzero(self._rule.get_valid_cards_from_obs(obs))
 
-            futures = []
-            for _ in range(self.n_threads):
-                future = self.executor.submit(_thread_search, action_scores, obs, self.limit_s)
-                futures.append(future)
+        #
+        action_scores = self.dmcts_worker.execute(obs)
 
-            for future in futures:
-                future.result()
-
-            all_action_scores = []
-            while not action_scores.empty():
-                all_action_scores.append(action_scores.get())
-
-            action_scores = np.array(all_action_scores)
-            mean_scores = np.mean(action_scores, axis=0)
-            best_card_index = np.argmax(mean_scores)
-            return int(valid_cards[best_card_index])
-
-
-def _thread_search(action_scores, game_obs, limit_s):
-    game_sim = __create_game_sim_from_obs(game_obs, RuleSchieber())
-    mcts = MCTS()
-    mcts.search(game_sim.state, limit_s=limit_s)
-
-    # sort by card index
-    mcts.root.children.sort(key=lambda x: x.card)
-    action_scores.put([child.score for child in mcts.root.children])
-
-
-def __create_game_sim_from_obs(game_obs: GameObservation, rule) -> GameSim:
-    game_sim = GameSim(rule=rule)
-    game_sim.init_from_state(state_from_observation(game_obs, HandSampler().sample(game_obs)))
-    return game_sim
+        mean_scores = np.mean(action_scores, axis=0)
+        best_card_index = np.argmax(mean_scores)
+        return int(valid_cards[best_card_index])
---
+++ src/play_strategy/nn/mcts/dmcts_worker.py
@@ -0,0 +1,66 @@
+from concurrent.futures import ProcessPoolExecutor
+from multiprocessing import Manager
+
+import numpy as np
+import psutil
+from jass.game.game_observation import GameObservation
+from jass.game.game_sim import GameSim
+from jass.game.game_state_util import state_from_observation
+from jass.game.rule_schieber import RuleSchieber
+
+from play_strategy.nn.mcts.hand_sampler import HandSampler
+from play_strategy.nn.mcts.mcts_tree import MCTS
+
+
+class DMCTSWorker:
+    def __init__(self, limit_s: float):
+        self._rule = RuleSchieber()
+        self.limit_s = limit_s
+        self.executor = ProcessPoolExecutor()
+
+    def execute(self, obs: GameObservation, n_determinations: int = None) -> np.ndarray:
+        """
+        Execute the determinized MCTS search. This method will return the action scores for each card in the hand.
+        :param obs: The game observation
+        :param n_determinations: The number of determinations to run in parallel. If None, the number of logical CPUs
+        :return: The action scores for each card in the hand
+        """
+        if n_determinations is None:
+            n_determinations = psutil.cpu_count(logical=False)
+
+        with Manager() as manager:
+            action_scores = manager.Queue()
+
+            futures = []
+            for _ in range(n_determinations):
+                future = self.executor.submit(_thread_search, action_scores, obs, self.limit_s)
+                futures.append(future)
+
+            for future in futures:
+                future.result()
+
+            all_action_scores = []
+            while not action_scores.empty():
+                all_action_scores.append(action_scores.get())
+
+            action_scores = np.array(all_action_scores)
+            return action_scores
+
+
+def _thread_search(action_scores, game_obs, limit_s):
+    """
+    Thread function to run the MCTS search
+    :param action_scores: A queue to put the action scores in
+    :param game_obs: The game observation
+    :param limit_s: The time limit for the search
+    :return: None
+    """
+
+    game_sim = GameSim(rule=RuleSchieber())
+    game_sim.init_from_state(state_from_observation(game_obs, HandSampler().sample(game_obs)))
+    mcts = MCTS()
+    mcts.search(game_sim.state, limit_s=limit_s)
+
+    # sort by card index
+    mcts.root.children.sort(key=lambda x: x.card)
+    action_scores.put([child.score for child in mcts.root.children])
