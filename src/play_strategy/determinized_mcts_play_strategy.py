f96b443eeb66094efdfc78fb0c5e795e97067ef4
Jakob Fender <jakob.fender@hslu.ch>
2024-11-06 17:56:10 +0100

    add example for distribution

--- src/play_strategy/determinized_mcts_play_strategy.py
+++ src/play_strategy/determinized_mcts_play_strategy.py
@@ -1,6 +1,8 @@
+import json
 from concurrent.futures.process import ProcessPoolExecutor
 
 import numpy as np
+import requests
 from jass.game.game_observation import GameObservation
 
 from play_strategy.nn.mcts.dmcts_worker import DMCTSWorker
@@ -19,7 +21,19 @@ class DeterminizedMCTSPlayStrategy(PlayStrategy):
     def choose_card(self, obs: GameObservation) -> int:
         valid_cards = np.flatnonzero(self._rule.get_valid_cards_from_obs(obs))
 
-        #
+        obs_json_str = json.dumps(obs.to_json())
+
+        # EXAMPLE CODE
+        # TODO: @Dan: Implement the following code with the nodes asynchronously
+        action_scores = []
+        node_list = []
+        for node in node_list:
+            response = requests.get(f"{node.url}/dmcts?obs={obs_json_str}")
+            action_scores.append(response.json())
+
+        # END EXAMPLE CODE
+
+        # Current code to keep the functionality for now
         action_scores = self.dmcts_worker.execute(obs)
 
         mean_scores = np.mean(action_scores, axis=0)
