f6ccad2a9fe98ac9d9f349216bfab22173e83d33
Jakob Fender <jakob.fender@hslu.ch>
2024-11-11 15:14:25 +0100

    remove unused variables

--- src/play_strategy/determinized_mcts_play_strategy.py
+++ src/play_strategy/determinized_mcts_play_strategy.py
@@ -1,13 +1,11 @@
 import json
 import logging
-from concurrent.futures.process import ProcessPoolExecutor
 import time
 
 import numpy as np
 from jass.game.game_observation import GameObservation
 
 from src.play_strategy.abstract_play_strategy import PlayStrategy
-from src.play_strategy.nn.mcts.dmcts_worker import DMCTSWorker
 from src.play_strategy.nn.mcts.hand_sampler import HandSampler
 from src.utils.worker_node_manager import WorkerNodeManager
 
@@ -17,8 +15,6 @@ class DeterminizedMCTSPlayStrategy(PlayStrategy):
         super().__init__(__name__)
         self.limit_s = limit_s
         self.hand_sampler = HandSampler()
-        self.executor = ProcessPoolExecutor()
-        self.dmcts_worker = DMCTSWorker(limit_s)
 
     def choose_card(self, obs: GameObservation) -> int:
         start_time = time.time()
